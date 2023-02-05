1ce35d9af42266d9b26b4c5334751e0411647bfc
Benjamin Brugghe <Brugghe.b@gmail.com>
2023-02-05 14:09:28 +0100

    Fix: modifications app-routing pour ajouter le loginForm sur la landing page

--- pokemon-store/src/app/app-routing.module.ts
+++ pokemon-store/src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RegisterFormComponent } from './components/register-form/register-form.component';
+import { LoginFormComponent } from './components/login-form/login-form.component';
 
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'Home',
-    pathMatch: 'full',
+    component: LoginFormComponent,
   },
   {
     path: 'register',
