59c0bdd456a3e1b6c611dd4ee441ec1977e2a2bb
Benjamin Brugghe <Brugghe.b@gmail.com>
2023-02-05 17:19:04 +0100

    Ajout de la méthode addToCart dans le composant pokemon-card

--- pokemon-store/src/app/components/pokemon-card/pokemon-card.component.ts
+++ pokemon-store/src/app/components/pokemon-card/pokemon-card.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input } from '@angular/core';
 import Pokemons from 'src/app/models/Pokemons';
+import Users from 'src/app/models/Users';
+import { ApiServiceService } from 'src/app/services/api-service.service';
 
 @Component({
   selector: 'app-pokemon-card',
@@ -9,4 +11,12 @@ import Pokemons from 'src/app/models/Pokemons';
 export class PokemonCardComponent {
   @Input()
   pokemon!: Pokemons;
+  @Input()
+  user!: Users;
+
+  private _service = new ApiServiceService();
+
+  public async addToCart(): Promise<void> {
+    await this._service.addToCart(this.user.id, this.pokemon);
+  }
 }
