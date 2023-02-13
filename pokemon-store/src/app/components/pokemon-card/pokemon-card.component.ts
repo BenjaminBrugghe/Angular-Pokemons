17c17bfa1329641231caf962131d6377fd0024e0
Benjamin Brugghe <Brugghe.b@gmail.com>
2023-02-13 17:34:35 +0100

    Fix: Ajout des popup à la place des alertes dans pokemon-card

--- pokemon-store/src/app/components/pokemon-card/pokemon-card.component.ts
+++ pokemon-store/src/app/components/pokemon-card/pokemon-card.component.ts
@@ -2,6 +2,7 @@ import { Component, Input } from '@angular/core';
 import Pokemons from 'src/app/models/Pokemons';
 import Users from 'src/app/models/Users';
 import { ApiServiceService } from 'src/app/services/api-service.service';
+import { SuccessPopupComponent } from '../snackbars/success-popup/success-popup.component';
 
 @Component({
   selector: 'app-pokemon-card',
@@ -16,9 +17,17 @@ export class PokemonCardComponent {
 
   private _service = new ApiServiceService();
 
+  public showSuccessPopup: boolean = false;
+
   public async addToCart(): Promise<void> {
     await this._service.addToCart(this.user.id, this.pokemon);
-    alert('Ce pokémon a bien été ajouté à votre panier !');
-    window.location.reload();
+
+    // Affiche la popup de succès
+    this.showSuccessPopup = true;
+
+    // Reset le booleen après 5s, pour pouvoir réafficher la popup sur le même pokemon
+    window.setTimeout(() => {
+      this.showSuccessPopup = false;
+    }, 5000);
   }
 }
