326f34d7484b48ba49b686ff3723edfdc35b4238
Benjamin Brugghe <Brugghe.b@gmail.com>
2023-02-16 13:55:39 +0100

    Fix: nom lastName et firstName dans le registerFOrm

--- pokemon-store/src/app/components/register-form/register-form.component.ts
+++ pokemon-store/src/app/components/register-form/register-form.component.ts
@@ -11,8 +11,8 @@ import { Router } from '@angular/router';
 })
 export class RegisterFormComponent {
   userForm: FormGroup = this.formBuilder.group({
-    firstName: ['', [Validators.required, Validators.minLength(3)]],
-    lastName: ['', [Validators.required, Validators.minLength(3)]],
+    firstname: ['', [Validators.required, Validators.minLength(3)]],
+    lastname: ['', [Validators.required, Validators.minLength(3)]],
     email: ['', [Validators.required, Validators.email]],
     password: ['', [Validators.required, Validators.minLength(6)]],
     confirmPassword: ['', [Validators.required, Validators.minLength(6)]],
@@ -40,8 +40,8 @@ export class RegisterFormComponent {
    */
   private createNewUser(): Users {
     return new Users(
-      this.userForm.value.firstName,
-      this.userForm.value.lastName,
+      this.userForm.value.firstname,
+      this.userForm.value.lastname,
       this.userForm.value.email,
       this.userForm.value.password
     );
