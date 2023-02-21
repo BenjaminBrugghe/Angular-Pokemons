75aebbdda31624833fb10354b3d242a6f8b90fec
Benjamin Brugghe <Brugghe.b@gmail.com>
2023-02-21 19:23:02 +0100

    Feat: Ajout tokenController et tokenService + Création du token

---
+++ Back-end-Angular-Pokemon/Controllers/TokenController.cs
@@ -0,0 +1,31 @@
+﻿using Back_end_Angular_Pokemon.Models;
+using Back_end_Angular_Pokemon.Services;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Back_end_Angular_Pokemon.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TokenController : ControllerBase
+    {
+        private TokenService _service = new TokenService();
+
+        // GET: api/<TokenController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // POST api/<TokenController>
+        [HttpPost]
+        public string CreateToken([FromBody] Utilisateurs user)
+        {
+            return _service.createToken(user);
+        }
+
+    }
+}
---
+++ Back-end-Angular-Pokemon/Services/TokenService.cs
@@ -0,0 +1,37 @@
+﻿using Back_end_Angular_Pokemon.Models;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Back_end_Angular_Pokemon.Services
+{
+    public class TokenService
+    {
+        public string createToken(Utilisateurs user)
+        {
+            // Instance de JwTSecurityTokenHandler
+            JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
+
+            // Configuration du Token
+            SecurityTokenDescriptor securityTokenDescriptor = new SecurityTokenDescriptor()
+            {
+                Expires = DateTime.Now.AddMinutes(60),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(Encoding.UTF8.GetBytes("mySecretKeymySecretKeymySecretKey")), SecurityAlgorithms.HmacSha256Signature),
+                Issuer = "MyIssuers",
+                Audience = "MyAudience",
+
+                // Création des role pour mon application
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                        new Claim("email", user.Email),
+                        new Claim("lastname", user.Lastname),
+                        new Claim("firstname", user.Firstname)
+                })
+            };
+            SecurityToken token = tokenHandler.CreateToken(securityTokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
